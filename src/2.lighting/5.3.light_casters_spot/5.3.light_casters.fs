d4f0a9a3ac5835939aaf5a535ec2c3a1c589602b
Han Qian <qianhan1986@msn.com>
2021-12-09 15:11:28 +0800

    update chapter 2.5.1 - 2.5.3

---
+++ src/2.lighting/5.3.light_casters_spot/5.3.light_casters.fs
@@ -0,0 +1,64 @@
+#version 330 core
+
+struct Material 
+{
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+}; 
+
+struct Light 
+{
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+uniform vec3 viewPos;
+uniform Light light;
+uniform Material material;
+
+void main()
+{
+    vec3 lightDir = normalize(light.position - FragPos);
+    float theta = dot(lightDir, normalize(-light.direction));
+    if(theta > light.cutOff )
+    {
+        // ambient
+        vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+        
+        // diffuse
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse  = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+        vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb; 
+
+        float distance = length(light.position - FragPos);
+        float attenuation = 1.0/(light.constant + light.linear * distance + light.quadratic * (distance * distance));
+        vec3 result = (ambient + diffuse + specular) * attenuation;
+        FragColor = vec4(result, 1.0);
+    } else
+    {
+        FragColor = vec4(light.ambient * vec3(texture(material.diffuse, TexCoords)), 1.0);
+    }
+}
